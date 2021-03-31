89374afb5841f2924109022626d5129b3e69dcbd
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-30 22:37:17 -0400

    wire Framing mockup

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -6,17 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1400, 800));
+        primaryStage.setTitle("Not Pictionary");
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module lab09 {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens sample;
-}
