493504379a4dbe749ce6e325afe42df1ed7cae89
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-31 21:51:31 -0400

    Implemented a player sidebar with programatically adding the players

--- src/main/java/client/Controller.java
+++ src/main/java/client/Controller.java
@@ -5,18 +5,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.CheckBox;
 import javafx.scene.canvas.GraphicsContext;
 
-
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.*;
 import java.net.Socket;
@@ -24,6 +20,7 @@ import java.net.Socket;
 public class Controller {
     @FXML private TextField chatInput;
     @FXML private ListView<String> chatMenu;
+    @FXML private ListView<String> playerMenu;
     @FXML private Button addBtn;
 
     @FXML private Canvas mainCanvas;
@@ -31,17 +28,24 @@ public class Controller {
     @FXML private CheckBox eraser;
     @FXML private TextField brushSize;
     @FXML private Button clearCanvas;
+    @FXML private VBox playerView;
+
+    @FXML private Spinner<Integer> BrushSize;
 
     @FXML private GraphicsContext gc;
 
     public void initialize() {
         gc = mainCanvas.getGraphicsContext2D();
-
+        //playerMenu.getItems().add("Players");
+        for (int i = 1; i <= 4; i++) {
+            addNewPlayer(i);
+        }
         addBtn.setOnAction(actionEvent -> addChat());
 
         mainCanvas.setOnMouseDragged(e -> {
-            String brush = brushSize.getText();
-            double size = Double.parseDouble(brush);
+            int brushWeight = BrushSize.getValue();
+//            String brush = brushSize.getText();
+            double size = Double.parseDouble(String.valueOf(brushWeight));
             double x = e.getX() - size / 2;
             double y = e.getY() - size / 2;
 
@@ -72,5 +76,13 @@ public class Controller {
     public void resetCanvas(GraphicsContext g) {
         g.clearRect(0, 0, mainCanvas.getWidth(), mainCanvas.getHeight());
     }
+
+    public void addNewPlayer(int i) {
+        Label label = new Label("PLAYER " + i);
+        HBox playerNode = new HBox(label);
+        playerNode.getStyleClass().add("testCell");
+        playerView.getChildren().add(playerNode);
+    }
+
 }
 
